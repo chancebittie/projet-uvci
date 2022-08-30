96e3b7497ecb808650ff6346fdbed8fae16f7a4c
chance <bsilas300@gmail.com>
2022-08-30 14:29:36 +0000

    partie connexion

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class Login extends Component
 {
+    public $email;
+    public $password;
+
+    protected $rules=[
+        "email"=>"required",
+        "password"=>"required|min:4",
+    ];
+
+    public function updated($propertedNane){
+        return $this->validateOnly($propertedNane);
+    }
     public function render()
     {
         return view('livewire.login');
